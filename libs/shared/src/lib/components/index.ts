1e7f06a28382b480eac12fc3fda667a1ad74e0ff
kaushal2469 <kaushal2469@gmail.com>
2025-06-12 16:04:41 +0530

    update index ts for shared components

--- libs/shared/src/lib/components/index.ts
+++ libs/shared/src/lib/components/index.ts
@@ -1,1 +1,4 @@
-export * from './priority-indicator/priority-indicator.component'
+export * from './due-date-indicator/due-date-indicator.component';
+export * from './priority-indicator/priority-indicator.component';
+export * from './progress-tracker/progress-tracker.component';
+export * from './status-badge/status-badge.component'
