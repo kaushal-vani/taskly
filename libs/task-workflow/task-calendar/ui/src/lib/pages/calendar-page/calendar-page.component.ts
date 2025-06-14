a5ed6304fdf062fb7d7f9473d24098019da19965
kaushal2469 <kaushal2469@gmail.com>
2025-06-14 23:11:57 +0530

    create calendar page

---
+++ libs/task-workflow/task-calendar/ui/src/lib/pages/calendar-page/calendar-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-calendar-page',
+  imports: [CommonModule],
+  templateUrl: './calendar-page.component.html',
+  styleUrl: './calendar-page.component.scss',
+})
+export class CalendarPageComponent {}
